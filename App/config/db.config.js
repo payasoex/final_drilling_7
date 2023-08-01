859a848b38de49d8c2332294c415559226b86fbe
retamal_f <javier.retamal.fontannaz@gmail.com>
2023-08-01 10:38:35 -0400

    Actualizacion de archivos

--- App/config/db.config.js
+++ App/config/db.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "user_db_bootcamp",
-    PASSWORD: "db_bootcamp",
-    DB: "db_bootcamp",
-    dialect: "postgres",
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000,
-        },
-    };
+  HOST: 'localhost',
+  USER: 'node_user',
+  PASSWORD: 'node_password',
+  DB: 'db_bootcamp',
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+}
